0977c34a5e7b4758a54fc7607e8ad717d3ae0b7f
Ben Napper <benjamin.napper17@gmail.com>
2023-10-18 09:21:44 -0400

    removed api key

--- src/hooks/useGeocode.js
+++ src/hooks/useGeocode.js
@@ -3,7 +3,7 @@ import { setKey, fromAddress } from 'react-geocode';
 export const useGeocode = () => {
     
     const geoCode = async (address) => {
-        setKey('AIzaSyAtyfghNNF2yljYpeg1n67qkodciewtKHs')
+        setKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
         
         const locationInfo = await fromAddress(address)
         const geoLocation = locationInfo.results[0].geometry.location;
